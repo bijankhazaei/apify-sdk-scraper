8ecfbab4da7b2c99b1e6443e7c5649c9534a3913
Samad -bijan- khzaeai <ceeprisk@gmail.com>
2021-11-05 02:27:39 +0330

    change post and add validator for url

--- index.js
+++ index.js
@@ -1,71 +1,70 @@
-const Apify = require ('apify');
-const express = require ('express')
-const bodyParser = require ('body-parser');
-const shops = require ('./util/shops');
-require ('dotenv').config ();
+const Apify = require("apify");
+const express = require("express");
+const bodyParser = require("body-parser");
+const shops = require("./util/shops");
+const { body, validationResult } = require("express-validator");
+require("dotenv").config();
 
 // Create ExpressJS app and configure body parsers that will allow us to receive form submissions.
-const app = express ();
+const app = express();
 const port = 3000;
 
-
-app.use (bodyParser.json ());
-app.use (bodyParser.urlencoded ({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //  Now we need to read following environment variables:
 // - APIFY_CONTAINER_PORT  contains a port number where we must start server
 // - APIFY_CONTAINER_URL  contains a URL under which we can access the container
 // - APIFY_DEFAULT_KEY_VALUE_STORE_ID is simply ID of default key-value store of this
 //   actor where we can store screenshots
-const {
-    APIFY_LOCAL_STORAGE_DIR,
-    APIFY_DEFAULT_KEY_VALUE_STORE_ID,
-} = process.env;
+const { APIFY_LOCAL_STORAGE_DIR, APIFY_DEFAULT_KEY_VALUE_STORE_ID } =
+  process.env;
 
 // Create an array of the processed URLs where n-th URL has its screenshot stored under the
 // key [n].jpg in key-value store:
 const processedUrls = [];
 
 // Root path displays a HTML form to submit new URL and thumbnails of processed URLs:
-app.get ('/', async (req, res) => {
-
-
-    const pageHtml = `
+app.get("/", async (req, res) => {
+  const pageHtml = `
     <html>
-    <head><title>Example</title></head>
+    <head><title>Apify</title></head>
     <body>
-        <form method="POST" action="http://localhost:${port}/add-url">
-            URL: <input type="text" name="url" placeholder="http://example.com" />
-            <input type="submit" value="Add" />
-        </form>
+        Hi There!
     </body>
     </html>`;
 
-    res.send (pageHtml);
+  res.send(pageHtml);
 });
 
 // POST route allowing us to submit new URLs. After URL is processed
 // it redirects user back to root path.
-app.post ('/add-url', async (req, res) => {
-    const {url} = req.body;
-    console.log (`Got new URL: ${url}`);
+app.post("/scrap", body("url").isURL(), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { url, title, price, image } = req.body;
 
-    const crawler = await shops.createCrawler (url);
+  if (url && title && price && image) {
+    const crawler = await shops.createCrawler(url, title, price, image);
 
-    await crawler.run ();
+    await crawler.run();
 
-    const dataset = await Apify.openDataset ('default');
+    const dataset = await Apify.openDataset("default");
 
-    const data = await dataset.getData ();
+    const data = await dataset.getData();
 
-    res.send (data.items[0] ?? {});
+    res.send(data.items[0] ?? {});
 
-    await dataset.drop ();
+    await dataset.drop();
 
-    await (await Apify.openKeyValueStore ()).drop ();
-
+    await (await Apify.openKeyValueStore()).drop();
+  } else {
+    res.send("input with error");
+  }
 });
 
-app.listen (port, () => {
-    console.log (`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- util/shops.js
+++ util/shops.js
@@ -1,57 +1,55 @@
-const Apify = require ('apify');
+const Apify = require("apify");
 
 const webSites = [
-    {
-        domain : 'ebay',
-        title : '#itemTitle',
-        price : '#prcIsum',
-        image_link : '#icImg',
+  {
+    domain: "ebay",
+    title: "#itemTitle",
+    price: "#prcIsum",
+    image_link: "#icImg",
+  },
+];
+
+const createCrawler = async (url, title, price, image) => {
+  const hostname = new URL(url).hostname;
+
+  const requestList = await Apify.openRequestList("my-request-list", [url]);
+
+  let titleSelector = title ?? ".title";
+  let priceSelector = price ?? ".price";
+  let imageLinkSelector = image ?? ".product-image";
+
+  webSites.forEach((elm, index) => {
+    console.log("elm :", elm);
+    if (hostname.indexOf(elm.domain) > 0) {
+      titleSelector = elm.title;
+      priceSelector = elm.price;
+      imageLinkSelector = elm.image_link;
     }
-]
-
-const createCrawler = async (url) => {
-    const hostname =  (new URL(url)).hostname;
-
-    const requestList = await Apify.openRequestList ('my-request-list', [
-        url
-    ], );
-
-    let titleSelector = '.title';
-    let priceSelector  = '.price';
-    let imageLinkSelector = '.product-image';
-
-    webSites.forEach((elm, index) => {
-        console.log ('elm :', elm);
-        if(hostname.indexOf(elm.domain) > 0) {
-             titleSelector = elm.title;
-             priceSelector = elm.price;
-             imageLinkSelector = elm.image_link;
-        }
-    });
-    return new Apify.PuppeteerCrawler ({
-        requestList,
-        handlePageFunction: async ({page, request}) => {
-            await Apify.pushData ({
-                title: await page.title(),
-                productTitle: await page.$eval(titleSelector , el => el.textContent),
-                price: await page.$eval(priceSelector , el => el.textContent),
-                image_link: await page.$eval(priceSelector, (elm) => {
-                    return elm.getAttribute('src')
-                }),
-                url: request.url,
-                succeeded: true,
-            });
-        },
-        handleFailedRequestFunction: async ({request}) => {
-            await Apify.pushData ({
-                url: request.url,
-                succeeded: false,
-                errors: request.errorMessages,
-            });
-        },
-    });
-}
+  });
+  return new Apify.PuppeteerCrawler({
+    requestList,
+    handlePageFunction: async ({ page, request }) => {
+      await Apify.pushData({
+        title: await page.title(),
+        productTitle: await page.$eval(titleSelector, (el) => el.textContent),
+        price: await page.$eval(priceSelector, (el) => el.textContent),
+        image_link: await page.$eval(priceSelector, (elm) => {
+          return elm.getAttribute("src");
+        }),
+        url: request.url,
+        succeeded: true,
+      });
+    },
+    handleFailedRequestFunction: async ({ request }) => {
+      await Apify.pushData({
+        url: request.url,
+        succeeded: false,
+        errors: request.errorMessages,
+      });
+    },
+  });
+};
 
 module.exports = {
-    createCrawler,
-}
+  createCrawler,
+};
